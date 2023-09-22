0e0a1bfd65f46b657165b36fd64def19ef8660e4
hasan <hasanocal7@gmail.com>
2023-09-22 22:52:08 +0300

    Added Single Course Page

--- controller/courseController.js
+++ controller/courseController.js
@@ -6,15 +6,51 @@ exports.createCourse = async (req, res) => {
       name: req.body.name,
       description: req.body.description,
     });
-    console.log('Kayıt başarılı')
     res.status(201).json({
       status: 'success',
       course,
     });
-  } catch {
+  } catch (error) {
     res.status(400).json({
       status: 'fail',
       error,
     })
   }
 };
+
+exports.getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find({});
+    /*
+    res.status(200).json({
+      status: 'success',
+      courses,
+    });*/
+
+    res.status(200).render('courses', {
+      courses,
+      page_name: 'courses'
+    })
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    })
+  }
+};
+
+exports.getCourse = async (req, res) => {
+  try {
+    const course = await Course.findOne({slug: req.params.slug});
+    res.status(200).render('course', {
+      course,
+      page_name: 'course'
+    })
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    })
+  }
+};
+
