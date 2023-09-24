398cdbf44b77678d513a3e758a7d1c8aca9bef9e
hasan <hasanocal7@gmail.com>
2023-09-24 14:21:02 +0300

    Added Login

--- controller/authController.js
+++ controller/authController.js
@@ -1,12 +1,10 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
 exports.createUser = async (req, res) => {
   try {
-    const user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
+    const user = await User.create(req.body);
     res.status(201).json({
       status: 'success',
       user,
@@ -16,5 +14,32 @@ exports.createUser = async (req, res) => {
       status: 'fail',
       error,
     })
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Kullanıcıyı email adresine göre bulun
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: 'Kullanıcı bulunamadı' });
+    }
+
+    // Şifreleri karşılaştırın
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: 'Şifre eşleşmiyor' });
+    }
+
+    // JWT oluşturun ve kullanıcıya gönderin
+    const token = jwt.sign({ userId: user._id }, 'your-secret-key', { expiresIn: '1h' });
+
+    res.status(200).json({ message: 'Başarıyla giriş yaptınız', token });
+  } catch (error) {
+    res.status(500).json({ message: 'Sunucu hatası', error: error.message });
   }
 };
--- controller/pageController.js
+++ controller/pageController.js
@@ -10,14 +10,14 @@ exports.getAboutPage = (req, res) => {
     })
 }
 
-exports.getCoursesPage = (req, res) => {
-    res.status(200).render('courses', {
-        page_name: "courses"
-      });
-}
-
 exports.getRegisterPage = (req, res) => {
     res.status(200).render('register', {
         page_name: "register"
+    });
+}
+
+exports.getLoginPage = (req, res) => {
+    res.status(200).render('login', {
+        page_name: "login"
     });
 }
