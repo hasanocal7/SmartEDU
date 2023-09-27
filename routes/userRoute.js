12ac2cc9ad0c7fec4ad055bd2416746d6aa403e1
hasan <hasanocal7@gmail.com>
2023-09-27 16:24:24 +0300

    Added Admin Panel and Bug Fixes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -23,6 +23,7 @@ router.route('/signup').post([
 ], authController.createUser);
 router.route('/login').post(authController.loginUser);
 router.route('/logout').get(authController.logoutUser);
+router.route('/:id').delete(authController.deleteUser);
 router.route('/dashboard').get(authMiddleware, authController.getDashboardPage);
 
 module.exports = router;
